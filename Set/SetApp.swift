a881224ac5295513519d88ddc7300af8b558acd9
xyzr <xyzr@qq.com>
2023-04-05 15:30:49 +0800

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by yuzhongbao on 2023/4/5.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
